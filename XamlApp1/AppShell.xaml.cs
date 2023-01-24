46cf4cfdade660a0145bcc861b85fad59dfc47bb
Kuvetov Daniil <kuvetov@bk.ru>
2023-01-24 11:31:46 +0300

    Добавьте файлы проекта.

---
+++ XamlApp1/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace XamlApp1;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ XamlApp1/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace XamlApp1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        this.BindingContext = new PhoneViewModel
+        {
+            Title = "iPhone 7",
+            Company = "Apple",
+            Price = 52000
+        };
+    }
+}
+
+
